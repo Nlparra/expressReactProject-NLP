5c941842408a9502981dd585f2e7d6079c04f41a
Nlparra <53584490+Nlparra@users.noreply.github.com>
2019-12-03 19:27:56 -0500

    Add SignUp functionality

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -1,21 +1,54 @@
 import React, { Component } from 'react'
 
 export default class SignUp extends Component {
+
+    state = {
+        email: '',
+        password:'',
+    }
+
+    handleChange = event => {
+        this.setState({[event.target.name]:event.target.value})
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        fetch('/users',{
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(this.state)
+        })
+
+    }
+
+
+
+
+
+
+
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <input 
+                    type="email" 
+                    className="form-control" aria-describedby="emailHelp" 
+                    name="email"
+                    onChange={this.handleChange}/>
                     <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
                 <div className="form-group">
                     <label htmlFor="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1"/>
-                </div>
-                <div className="form-group form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                    <input 
+                    type="password" 
+                    className="form-control" 
+                    name="password"
+                    onChange={this.handleChange}/>
                 </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
             </form>
